8ae820c7a7aad3d8c2fcf69ccea932347e0515fb
bolin <colinbolin@126.com>
2020-09-20 23:04:23 +0800

    Week 06 homework

---
+++ Week_06/MaxSquare.java
@@ -0,0 +1,43 @@
+public class MaxSquare {
+
+    public int maximalSquare(char[][] matrix) {
+        int max = 0;
+
+        int m = matrix.length;
+        if (m == 0) return 0;
+
+        int n = matrix[0].length;
+        if (n == 0) return 0;
+
+        int[][] dp = new int[m][n];
+
+        for (int i = 0; i < m; i++){
+            dp[i][0] = matrix[i][0] - 48;
+            if (dp[i][0] == 1) max = 1;
+        }
+
+        for (int i = 0; i < n; i++){
+            dp[0][i] = matrix[0][i] - 48;
+            if (dp[0][i] == 1) max = 1;
+        }
+
+        for (int i = 1; i < m; i++){
+            for (int j = 1; j < n; j++){
+                if (matrix[i][j] == '0'){
+                    dp[i][j] = 0;
+                }
+                else {
+                    dp[i][j] = Math.min(dp[i - 1][j], Math.min(dp[i][j - 1], dp[i - 1][j - 1])) + 1;
+                    max = Math.max(max, dp[i][j]);
+                }
+            }
+        }
+
+        return max * max;
+    }
+
+    public static void main(String[] args) {
+        char[][] matrix = {{'1','0','1','0','0'},{'1','0','1','1','1'},{'1','1','1','1','1'},{'1','0','0','1','0'}};
+        System.out.println(new MaxSquare().maximalSquare(matrix));
+    }
+}
--- Week_06/README.md
+++ Week_06/README.md
@@ -1,1 +1,13 @@
 学习笔记
+
+Dynamic Programming:
+
+Divide the problem into sub problems. Approach the final result step by step.
+
+1. Define the sub problems
+
+2. Define the solutions to the sub problems
+
+3. Run recursion or memorize the steps
+
+4. Solve the original problem
